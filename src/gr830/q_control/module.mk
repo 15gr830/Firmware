d41916d9ace6f03b5ac8c3341518a67940279421
Brian Gasberg Thomsen <mail@briangasberg.dk>
2015-05-22 09:29:37 +0200

    bgt

--- src/gr830/q_control/module.mk
+++ src/gr830/q_control/module.mk
@@ -5,5 +5,5 @@
 MODULE_COMMAND	= q_control
 SRCS		= q_control_main.cpp
 MODULE_STACKSIZE = 3000
-EXTRACFLAGS = -Wno-float-equal -Wframe-larger-than=3700
+EXTRACFLAGS = -Wno-float-equal -Wframe-larger-than=5000
 EXTRACXXFLAGS = -Wframe-larger-than=2400
